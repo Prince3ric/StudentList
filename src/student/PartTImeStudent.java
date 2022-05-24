2606f5c2c74e6f961bdf14020c45d33868424cfd
eric mogielnicki <mogielne@sheridancollege.ca>
2022-05-24 14:07:20 -0400

    add part time branch

---
+++ src/student/PartTImeStudent.java
@@ -0,0 +1,17 @@
+package student;
+
+/**
+ * This class +++ Insert Description Here +++
+ * 
+ * @author Eric Mogielnicki
+ */
+public class PartTImeStudent extends Student {
+    
+    private int numOfCourses;
+
+
+
+
+    
+
+}
